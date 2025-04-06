ac824454d34e7d4ba4c2482986d9f95187ef04f6
ViosRio <erollitex@gmail.com>
2025-04-07 02:41:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,16 +14,16 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/2030bea6c41b37ec65a8d.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/b30b497dd011204136373.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/2b4e22e24548f55f40e65.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/2b4e22e24548f55f40e65.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/destekgroup")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PlutoKanal")
-PLAYLIST = getenv("PLAYLIST", "https://t.me/PlutoFm")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Bot4Chan")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/CerenFm")
+PLAYLIST = getenv("PLAYLIST", "https://t.me/CerenFm")
 
 PLAYLIST_ID = int(getenv("PLAYLIST_ID", ""))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/file/2b4e22e24548f55f40e65.jpg"
